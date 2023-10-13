d4125bdc7d248cc89b44d322f3a25bb83918fba3
dcooperdalrymple <dcooperdalrymple@gmail.com>
2023-10-13 10:57:57 -0500

    Added offset sine wave and noise variants.

--- circuitpython/src/waveform.py
+++ circuitpython/src/waveform.py
@@ -25,6 +25,12 @@ class Waveform:
         return Waveform._sine
 
     @staticmethod
+    def get_offset_sine():
+        if not hasattr(Waveform, "_offset_sine"):
+            Waveform._offset_sine = Waveform._get_sine(0.5)
+        return Waveform._offset_sine
+
+    @staticmethod
     def get_square():
         if not hasattr(Waveform, "_square"):
             Waveform._square = numpy.concatenate((numpy.ones(Waveform.get_samples()//2, dtype=numpy.int16)*Waveform.get_amplitude(),numpy.ones(Waveform.get_samples()//2, dtype=numpy.int16)*-Waveform.get_amplitude()))
@@ -35,3 +41,19 @@ class Waveform:
         if not hasattr(Waveform, "_noise"):
             Waveform._noise = numpy.array([random.randint(-Waveform.get_amplitude(), Waveform.get_amplitude()) for i in range(Waveform.get_samples())], dtype=numpy.int16)
         return Waveform._noise
+
+    @staticmethod
+    def get_sine_noise():
+        if not hasattr(Waveform, "_sine_noise"):
+            Waveform.get_sine()
+            Waveform.get_noise()
+            Waveform._sine_noise = numpy.array([int(max(min(Waveform._sine[i] + (Waveform._noise[i]/2.0), Waveform.get_amplitude()), -Waveform.get_amplitude())) for i in range(Waveform.get_samples())], dtype=numpy.int16)
+        return Waveform._sine_noise
+
+    @staticmethod
+    def get_offset_sine_noise():
+        if not hasattr(Waveform, "_offset_sine_noise"):
+            Waveform.get_offset_sine()
+            Waveform.get_noise()
+            Waveform._offset_sine_noise = numpy.array([int(max(min(Waveform._offset_sine[i] + (Waveform._noise[i]/2.0), Waveform.get_amplitude()), -Waveform.get_amplitude())) for i in range(Waveform.get_samples())], dtype=numpy.int16)
+        return Waveform._offset_sine_noise
