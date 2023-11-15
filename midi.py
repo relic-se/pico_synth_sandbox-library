8132a3843fe167e66699bc130303572b327d6905
dcooperdalrymple <dcooperdalrymple@gmail.com>
2023-11-15 08:36:30 -0600

    Improved midi channel and thru support.

--- midi.py
+++ midi.py
@@ -13,13 +13,16 @@ from adafruit_midi.note_off import NoteOff
 from adafruit_midi.control_change import ControlChange
 from adafruit_midi.pitch_bend import PitchBend
 from adafruit_midi.program_change import ProgramChange
+from adafruit_midi.midi_message import MIDIUnknownEvent
 
 class Midi:
     """Send and receive both hardware UART and USB MIDI messages using :class:`adafruit_midi.MIDI`. UART can be enabled with the `MIDI_UART` variable and USB can be enabled with the `MIDI_USB` variable in `settings.toml`. The midi channel is limited to a single value for both input and output and is determined by the `MIDI_CHANNEL` variable in `settings.toml` with a range of 0-15. However, the channel can be changed once a :class:`pico_synth_sandbox.midi.Midi` object is created by calling the `set_channel` function. By default, the onboard led will be used to indicate incoming midi messages. At the moment, this feature cannot be disabled.
     """
 
     def __init__(self):
+        self._channel = None
         self._thru = False
+
         self._note_on = None
         self._note_off = None
         self._control_change = None
@@ -36,8 +39,6 @@ class Midi:
             self._uart_midi = adafruit_midi.MIDI(
                 midi_in=self._uart,
                 midi_out=self._uart,
-                in_channel=os.getenv("MIDI_CHANNEL", 0),
-                out_channel=os.getenv("MIDI_CHANNEL", 0),
                 debug=False
             )
         else:
@@ -47,8 +48,6 @@ class Midi:
             self._usb_midi = adafruit_midi.MIDI(
                 midi_in=usb_midi.ports[0],
                 midi_out=usb_midi.ports[1],
-                in_channel=os.getenv("MIDI_CHANNEL", 0),
-                out_channel=os.getenv("MIDI_CHANNEL", 0),
                 debug=False
             )
         else:
@@ -99,15 +98,14 @@ class Midi:
     def set_channel(self, value):
         """Set the midi channel for messages to be received and sent from.
 
-        :param value: The desired channel from 0 to 15.
+        :param value: The desired channel from 0 to 16. 0 will accept all midi messages.
         :type value: int
         """
-        if self._uart_midi:
-            self._uart_midi.in_channel = value
-            self._uart_midi.out_channel = value
-        if self._usb_midi:
-            self._usb_midi.in_channel = value
-            self._usb_midi.out_channel = value
+        if value == 0 or value is None:
+            value = None
+        else:
+            value = clamp(value - 1, 0, 15)
+        self._channel = value
     def set_thru(self, value):
         """Set whether you would like to forward incoming midi messages through the enabled outputs automatically.
 
@@ -119,27 +117,28 @@ class Midi:
     def _process_message(self, msg):
         if not msg:
             return
+        
+        if self._channel is None or msg.channel == self._channel:
+            if isinstance(msg, NoteOn):
+                if msg.velocity > 0.0:
+                    if self._note_on:
+                        self._note_on(msg.note, msg.velocity / 127.0)
+                elif self._note_off:
+                    self._note_off(msg.note)
+            elif isinstance(msg, NoteOff):
+                if self._note_off:
+                    self._note_off(msg.note)
+            elif isinstance(msg, ControlChange):
+                if self._control_change:
+                    self._control_change(msg.control, msg.value / 127.0)
+            elif isinstance(msg, PitchBend):
+                if self._pitch_bend:
+                    self._pitch_bend((msg.pitch_bend - 8192) / 8192)
+            elif isinstance(msg, ProgramChange):
+                if self._program_change:
+                    self._program_change(msg.patch)
 
-        if isinstance(msg, NoteOn):
-            if msg.velocity > 0.0:
-                if self._note_on:
-                    self._note_on(msg.note, msg.velocity / 127.0)
-            elif self._note_off:
-                self._note_off(msg.note)
-        elif isinstance(msg, NoteOff):
-            if self._note_off:
-                self._note_off(msg.note)
-        elif isinstance(msg, ControlChange):
-            if self._control_change:
-                self._control_change(msg.control, msg.value / 127.0)
-        elif isinstance(msg, PitchBend):
-            if self._pitch_bend:
-                self._pitch_bend((msg.pitch_bend - 8192) / 8192)
-        elif isinstance(msg, ProgramChange):
-            if self._program_change:
-                self._program_change(msg.patch)
-
-        if self._thru:
+        if self._thru and not isinstance(msg, MIDIUnknownEvent):
             if self._uart_midi:
                 self._uart_midi.send(msg)
             if self._usb_midi:
@@ -179,10 +178,11 @@ class Midi:
         :type velocity: float
         """
         velocity = int(clamp(velocity) * 127.0)
+        msg = NoteOn(notenum, velocity, channel=self._channel)
         if self._uart_midi:
-            self._uart_midi.send(NoteOn(notenum, velocity))
+            self._uart_midi.send(msg)
         if self._usb_midi:
-            self._usb_midi.send(NoteOn(notenum, velocity))
+            self._usb_midi.send(msg)
         self._trigger_led()
     def send_note_off(self, notenum):
         """Send an :class:`adafruit_midi.note_off.NoteOff` message through the enabled midi outputs.
@@ -190,10 +190,11 @@ class Midi:
         :param notenum: The value of the midi note to send.
         :type notenum: int
         """
+        msg = NoteOff(notenum, channel=self._channel)
         if self._uart_midi:
-            self._uart_midi.send(NoteOff(notenum))
+            self._uart_midi.send(msg)
         if self._usb_midi:
-            self._usb_midi.send(NoteOff(notenum))
+            self._usb_midi.send(msg)
         self._trigger_led()
     def send_control_change(self, control, value):
         """Send an :class:`adafruit_midi.control_change.ControlChange` message through the enabled midi outputs.
@@ -204,8 +205,9 @@ class Midi:
         :type value: float
         """
         value = int(clamp(value) * 127.0)
+        msg = ControlChange(control, value, channel=self._channel)
         if self._uart_midi:
-            self._uart_midi.send(ControlChange(control, value))
+            self._uart_midi.send(msg)
         if self._usb_midi:
-            self._usb_midi.send(ControlChange(control, value))
+            self._usb_midi.send(msg)
         self._trigger_led()
