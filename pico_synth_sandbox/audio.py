b36b1dac540703b0233d35285522eff30c24f784
dcooperdalrymple <dcooperdalrymple@gmail.com>
2024-01-17 13:08:56 -0600

    Enable reconfiguration of the audio driver and more granular control of mixer voices.

--- pico_synth_sandbox/audio.py
+++ pico_synth_sandbox/audio.py
@@ -12,24 +12,104 @@ class Audio:
 
     :param output: An :class:`audioio.AudioOut` object
     :type output: class:`audioio.AudioOut`
-    :param count: The number of voices to create for the audio mixer
-    :type count: int
+    :param voice_count: The number of voices to create for the audio mixer
+    :type voice_count: int
     """
 
-    def __init__(self, output, count=1):
+    def __init__(self, output, voice_count=1, channel_count=2, sample_rate=None, buffer_size=None):
         """Constructor method
         """
-        if count < 1: count = 1
-        self._mixer = Mixer(
-            voice_count=count,
-            sample_rate=Audio.get_sample_rate(),
-            channel_count=2,
-            bits_per_sample=16,
-            samples_signed=True,
-            buffer_size=Audio.get_buffer_size()
-        )
         self._output = output
+        self.configure(voice_count, channel_count, sample_rate, buffer_size)
+
+    def configure(self, voice_count=1, channel_count=2, sample_rate=None, buffer_size=None, bits_per_sample=16):
+        # Validate parameters
+        if voice_count < 1: voice_count = 1
+        if channel_count < 1: channel_count = 1
+        if sample_rate is None: sample_rate = os.getenv("AUDIO_RATE", 22050)
+        if buffer_size is None: buffer_size = os.getenv("AUDIO_BUFFER", 2048)
+
+        # Stop existing objects
+        if self._output.playing:
+            self._output.stop()
+        if hasattr(self, "_mixer") and not self._mixer is None:
+            for voice in self._mixer.voice:
+                if voice.playing:
+                    voice.stop()
+            self._mixer = None
+        
+        # Store parameters
+        self._voice_count = voice_count
+        self._channel_count = channel_count
+        self._sample_rate = sample_rate
+        self._buffer_size = buffer_size
+        self._bits_per_sample = bits_per_sample
+        
+        # Create Mixer object and attach to audio output
+        self._mixer = Mixer(
+            voice_count=self._voice_count,
+            channel_count=self._channel_count,
+            sample_rate=self._sample_rate,
+            buffer_size=self._buffer_size,
+            bits_per_sample=self._bits_per_sample,
+            samples_signed=True
+        )
         self._output.play(self._mixer)
+
+    def set_voice_count(self, value):
+        self.configure(
+            voice_count=value,
+            channel_count=self._channel_count,
+            sample_rate=self._sample_rate,
+            buffer_size=self._buffer_size,
+            bits_per_sample=self._bits_per_sample
+        )
+    def get_voice_count(self):
+        return len(self._mixer.voice)
+    
+    def set_channel_count(self, value):
+        self.configure(
+            voice_count=self._voice_count,
+            channel_count=value,
+            sample_rate=self._sample_rate,
+            buffer_size=self._buffer_size,
+            bits_per_sample=self._bits_per_sample
+        )
+    def get_channel_count(self):
+        return self._channel_count
+
+    def set_sample_rate(self, value):
+        self.configure(
+            voice_count=self._voice_count,
+            channel_count=self._channel_count,
+            sample_rate=value,
+            buffer_size=self._buffer_size,
+            bits_per_sample=self._bits_per_sample
+        )
+    def get_sample_rate(self):
+        return self._mixer.sample_rate
+
+    def set_buffer_size(self, value):
+        self.configure(
+            voice_count=self._voice_count,
+            channel_count=self._channel_count,
+            sample_rate=self._sample_rate,
+            buffer_size=value,
+            bits_per_sample=self._bits_per_sample
+        )
+    def get_buffer_size(self):
+        return self._buffer_size
+
+    def set_bits_per_sample(self, value):
+        self.configure(
+            voice_count=self._voice_count,
+            channel_count=self._channel_count,
+            sample_rate=self._sample_rate,
+            buffer_size=self._buffer_size,
+            bits_per_sample=value
+        )
+    def get_bits_per_sample(self):
+        return self._bits_per_sample
 
     def set_level(self, value, index=-1):
         """Set the level of output of all or a single mixer voice.
@@ -56,6 +136,24 @@ class Audio:
         if index >= 0 and index < len(self._mixer.voice):
             self._mixer.voice[index].play(source)
 
+    def stop(self, index=-1):
+        if index >= 0 and index < len(self._mixer.voice):
+            if self._mixer.voice[index].playing:
+                self._mixer.voice[index].stop()
+        elif index < 0:
+            for voice in self._mixer.voice:
+                if voice.playing:
+                    voice.stop()
+    
+    def is_playing(self, index=-1):
+        if index >= 0 and index < len(self._mixer.voice):
+            return self._mixer.voice[index].playing
+        elif index < 0:
+            for voice in self._mixer.voice:
+                if voice.playing:
+                    return True
+            return False
+
     def mute(self):
         self._output.pause()
     def unmute(self):
@@ -67,24 +165,6 @@ class Audio:
             self.unmute()
         else:
             self.mute()
-
-    @staticmethod
-    def get_sample_rate():
-        """Returns the sample rate of the audio mixer as defined by the settings.toml file.
-
-        :return: sample rate
-        :rtype: int
-        """
-        return os.getenv("AUDIO_RATE", 22050)
-
-    @staticmethod
-    def get_buffer_size():
-        """Returns the buffer size of the audio mixer as defined by the settings.toml file.
-
-        :return: buffer size
-        :rtype: int
-        """
-        return os.getenv("AUDIO_BUFFER", 2048)
 
 class I2SAudio(Audio):
     """This class helps manage audio output and mixing using an :class:`audioio.AudioOut` object of type :class:`audiobusio.I2SOut`.
@@ -127,8 +207,3 @@ def get_audio_driver(count=1):
         return I2SAudio(count)
     else:
         return PWMAudio(count)
-
-MIN_FILTER_FREQUENCY = 60.0
-MAX_FILTER_FREQUENCY = min(20000.0, Audio.get_sample_rate() * 0.45)
-MIN_FILTER_RESONANCE = 0.7071067811865475
-MAX_FILTER_RESONANCE = 8.0
