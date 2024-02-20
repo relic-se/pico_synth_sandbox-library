925deb972caad31667093e5af1eed2d897ec5bb2
dcooperdalrymple <dcooperdalrymple@gmail.com>
2024-02-20 12:37:11 -0600

    Use PIO for TTP229 serial RX.

--- pico_synth_sandbox/keyboard/ton_touch.py
+++ pico_synth_sandbox/keyboard/ton_touch.py
@@ -5,8 +5,11 @@
 # NOTE: If using module, close jumpers TP3 & TP4 for multi-key output, TP1 for serial interface (active-high), and TP2 for 16-key inputs (leave open for 8-key).
 
 from pico_synth_sandbox.keyboard import DebouncerKey, Keyboard
+import rp2pio
+import adafruit_pioasm
+import array
 
-tontouch_data = 0
+tontouch_data = array.array('H', [0])
 
 class TonTouchPad(DebouncerKey):
     def __init__(self, index):
@@ -16,7 +19,7 @@ class TonTouchPad(DebouncerKey):
 
     def read(self):
         global tontouch_data
-        return bool(tontouch_data & self._bit)
+        return bool(tontouch_data[0] & self._bit)
 
 class TonTouchKeyboard(Keyboard):
 
@@ -26,7 +29,7 @@ class TonTouchKeyboard(Keyboard):
     def __init__(self, board, max_voices=1, root=None, input_mode=MODE_16KEY, invert_clk=True):
         self._input_mode = input_mode
         self._input_bits = (input_mode + 1) * 8
-        self._invert_clk = invert_clk
+        #self._invert_clk = invert_clk
 
         Keyboard.__init__(
             self,
@@ -34,10 +37,47 @@ class TonTouchKeyboard(Keyboard):
             max_voices=max_voices,
             root=root
         )
-        self.set_update_frequency(128)
 
-        self._sdo, self._scl = board.get_ttp()
-        self._scl.value = self._invert_clk
+        clk_off = 1 if invert_clk else 0
+        clk_on = 0 if invert_clk else 0
+        clk_cnt = self._input_bits - 1
+        pioasm = f"""
+.program read_ttp
+    set pins, {clk_off}
+.wrap_target
+    set y, 3
+tout_y:
+    set x, 31
+tout_x:
+    nop [31]
+    jmp x-- tout_x
+    jmp y-- tout_y
+    set x, {clk_cnt}
+bitloop:
+    set pins, {clk_on} [3]
+    set pins, {clk_off} [1]
+    in pins, 1
+    jmp x-- bitloop
+    push
+.wrap
+"""
+        self._piosm = rp2pio.StateMachine(
+            adafruit_pioasm.assemble(pioasm),
+            frequency=2000000, # 2MHz, cycle = 0.5us
+            first_in_pin=board.ttp_sdo,
+            in_pin_count=1,
+            pull_in_pin_up=1,
+            first_set_pin=board.ttp_scl,
+            set_pin_count=1,
+            initial_set_pin_state=clk_off,
+            initial_set_pin_direction=1,
+        )
+        # Timing Details:
+        # Clock Cycle (F_SCL) = 8 pio cycles = 4us = 250KHz
+        # Word Cycle = 64us = ~15.6KHz
+        # Delay (Tout) = 2ms
+        # Frequency (T_resp) = 2064us = ~484.5Hz
+        self.set_update_frequency(484)
 
     async def update(self):
         self.read_data()
@@ -45,13 +85,5 @@ class TonTouchKeyboard(Keyboard):
 
     def read_data(self):
         global tontouch_data
-        tontouch_data = 0
-
-        # Clock is around 2.8khz
-        self._scl.value = not self._invert_clk
-        for i in range(self._input_bits):
-            self._scl.value = self._invert_clk
-            if self._sdo.value:
-                tontouch_data |= (1 << i)
-            self._scl.value = not self._invert_clk
-        self._scl.value = self._invert_clk
+        if self._piosm.in_waiting <= 0: return
+        self._piosm.readinto(tontouch_data)
