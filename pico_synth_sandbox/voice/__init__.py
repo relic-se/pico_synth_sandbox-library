0cf796e61a9fbdcc8f9be80ef1311a5a15fa1bef
Cooper Dalrymple <me@dcdalrymple.com>
2024-02-23 08:09:50 -0600

    Documentation for pico_synth_sandbox.voice.LerpBlockInput.

--- pico_synth_sandbox/voice/__init__.py
+++ pico_synth_sandbox/voice/__init__.py
@@ -7,7 +7,17 @@ import ulab.numpy as numpy
 import synthio
 
 class LerpBlockInput:
-    def __init__(self, rate=0.05, value=0.0):
+    """Creates and manages a :class:`synthio.BlockInput` object to "lerp" (linear interpolation) between an old value and a new value. Useful for note frequency "glide" and custom envelopes.
+
+    :param rate: The speed at which to go between values, in seconds. Must be greater than 0.0s. Defaults to 0.05s.
+    :type rate: float
+    :param value: The initial value. Defaults to 0.0.
+    :type value: float
+    """
+
+    def __init__(self, rate:float=0.05, value:float=0.0):
+        """Constructor method
+        """
         self.position = synthio.LFO(
             waveform=numpy.linspace(-16385, 16385, num=2, dtype=numpy.int16),
             rate=1/rate,
@@ -16,19 +26,55 @@ class LerpBlockInput:
             once=True
         )
         self.lerp = synthio.Math(synthio.MathOperation.CONSTRAINED_LERP, value, value, self.position)
-    def get(self):
+
+    def get(self) -> synthio.Math:
+        """Get the block input to be used with a :class:`synthio.Note` object.
+
+        :return: linear interpolation block input
+        :rtype: :class:`synthio.Math`
+        """
         return self.lerp
-    def get_blocks(self):
+    
+    def get_blocks(self) -> list[synthio.BlockInput]:
+        """Get all blocks used by this object. In order for it to function properly, these blocks must be added to the primary :class:`synthio.Synthesizer` object using `synth.blocks.append(...)` or to a :class:`pico_synth_sandbox.synth.Synth` object using `synth.append(...)`.
+
+        :return: list of blocks
+        :rtype: list[:class:`synthio.BlockInput`]
+        """
         return [self.position, self.lerp]
-    def get_value(self):
+    
+    def get_value(self) -> float:
+        """Get the current value of the linear interpolation output.
+
+        :return: interpolated value
+        :rtype: float
+        """
         return self.lerp.value
-    def set(self, value):
+    
+    def set(self, value:float):
+        """Set a new value to interpolate to from the current value state. Causes the interpolation process to retrigger.
+
+        :param value: new value
+        :type value: float
+        """
         self.lerp.a = self.lerp.value
         self.lerp.b = value
         self.position.retrigger()
-    def set_rate(self, value):
+
+    def set_rate(self, value:float):
+        """Change the rate at which the interpolation occurs, in seconds. Must be greater than 0.0s.
+
+        :param value: Amount of time it takes to interpolate between an old value and a new value in seconds.
+        :type value: float
+        """
         self.position.rate = 1/max(value, 0.001)
-    def get_rate(self):
+
+    def get_rate(self) -> float:
+        """Get the current rate of change of interpolation. Returns as 1/seconds.
+
+        :return: rate of change per second
+        :rtype: float
+        """
         return self.position.rate
 
 class AREnvelope:
@@ -74,6 +120,9 @@ class AREnvelope:
         self._pressed = False
 
 class Voice:
+    """A "voice" to be used with a :class:`pico_synth_sandbox.synth.Synth` object. Manages one or multiple :class:`synthio.Note` objects to be used with the primary :class:`synthio.Synthesizer` object.
+    """
+    
     def __init__(self):
         self._notenum = -1
         self._velocity = 0.0
